356822d2e66a0482ee9852f47c8fc1625d097fdf
Horix21 <horiaradu80@gmail.com>
2024-06-21 02:11:18 +0300

    Small bug fix, added a few more types of tiles and implemented high score system. Will add encryption for it later

--- Game.py
+++ Game.py
@@ -1,18 +1,36 @@
+from errno import WSAEDESTADDRREQ
+import os
+from tkinter import W
 import pygame
 import pygame.freetype
 import random as rand
 
 pygame.init()
 
-SCREEN_WIDTH = 800
+SCREEN_WIDTH = 900
 SCREEN_HEIGHT = 900
+
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('BlockGame')
 pygame.font.init()
 my_font = pygame.freetype.Font('Mont.otf', 30)
 
-
+hs = ''
+if(os.path.exists("highscore.txt")):
+    f = open("highscore.txt", 'r+')
+else:
+    open("highscore.txt", 'w+')
+    f = open("highscore.txt", 'r+')
+hs = f.read()
+if(not hs):
+    hs = 0
+elif(not hs.isnumeric()):
+    print("Something is wrong with highscore file, creating a new one")
+    f = open("highscore.txt", 'w')
+    f.write('0')
+    hs = 0
+hs= int(hs)
 
 block = pygame.Rect((0, 0, 0, 0))
 blockrect = pygame.Rect((0, 0, 0, 0))
@@ -41,7 +59,9 @@ class blockClass:
 blocks = [blockClass([0, 0, 0, 0 ], [-2, -1, 0, 1]), #long1
           blockClass([2, 1, 0, -1], [0, 0, 0, 0]),#long2
           blockClass([1, 0, -1], [0, 0, 0]), #long3
-          blockClass([0, 0, 0], [-1, 0, 1]), #long4 
+          blockClass([0, 0, 0], [-1, 0, 1]), #long4
+          blockClass([0, 0], [0, 1]), #short1
+          blockClass([0, 1], [0, 0]), #short2
           blockClass([0, 0, 0, 1], [-1, 0, 1, 1]),#l1
           blockClass([1, 1, 0, -1], [-1, 0 , 0 ,0]),#l2
           blockClass([-1, 0, 1, 1], [0, 0, 0, -1]),#l3
@@ -56,7 +76,7 @@ blocks = [blockClass([0, 0, 0, 0 ], [-2, -1, 0, 1]), #long1
 rectangles = []
 
 def drawGrid():
-    for x in range(200, 600, blockSize):
+    for x in range(300, 700, blockSize):
         for y in range(150, 550, blockSize):
             rect = pygame.Rect(x, y, blockSize, blockSize)
             pygame.draw.rect(screen, (255, 255, 255), rect, 1)
@@ -66,7 +86,7 @@ def drawCurrentBlock(block):
     for x in block.formationx:
         
         if(not isClicked):
-            blockrect = pygame.Rect(375 + block.formationx[i] * blockSize, 750 + block.formationy[i] * blockSize, blockSize, blockSize)
+            blockrect = pygame.Rect(475 + block.formationx[i] * blockSize, 750 + block.formationy[i] * blockSize, blockSize, blockSize)
             pygame.draw.rect(screen, (255, 255, 255), blockrect)
         x, y = pygame.mouse.get_pos()
         if(isClicked):
@@ -75,8 +95,8 @@ def drawCurrentBlock(block):
         i = i + 1
         
 def drawButton():
-    my_font.render_to(screen, (637, 712), f"Reset", (255, 255, 255))
-    buttonRect = pygame.Rect(625, 700, 100, 50)
+    my_font.render_to(screen, (737, 712), f"Reset", (255, 255, 255))
+    buttonRect = pygame.Rect(725, 700, 100, 50)
     pygame.draw.rect(screen, (255, 255, 255), buttonRect, 1)
     
 def reset():
@@ -85,15 +105,17 @@ def reset():
             map[j][k] = 0
            
 def drawBox():
-    box = pygame.Rect(275, 600, 250, 250)
+    box = pygame.Rect(375, 600, 250, 250)
     pygame.draw.rect(screen, (255, 255, 255), box, 1)
 
-currentRect = blocks[rand.randint(0, 9)]
+currentRect = blocks[rand.randint(0, len(blocks)-1)]
 run = True
 while run:
     
     screen.fill((0, 0, 0))
     my_font.render_to(screen, (40, 350), f"Score: {score}", (255, 255, 255))
+    my_font.render_to(screen, (40, 375), f"High Score: {hs}", (255, 255, 255))
+    if(score > hs): hs = score
     drawGrid()
     drawButton()
     drawBox()
@@ -101,16 +123,16 @@ while run:
     for j in range(8):
         for k in range(8):
             if (map[k][j]):
-                rect = pygame.Rect( 200 + j * blockSize, 150 + k * blockSize, blockSize, blockSize)
+                rect = pygame.Rect( 300 + j * blockSize, 150 + k * blockSize, blockSize, blockSize)
                 pygame.draw.rect(screen, (255, 255, 255), rect)
     
 
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             x, y = pygame.mouse.get_pos()
-            if(x > 275 and x < 525 and y > 600 and y < 850):
+            if(x > 375 and x < 625 and y > 600 and y < 850):
                 isClicked = True
-            if(x > 625 and x < 725 and y > 700 and y < 750):
+            if(x > 725 and x < 825 and y > 700 and y < 750):
                 reset()
                 score = 0
         
@@ -119,10 +141,10 @@ while run:
             x, y = pygame.mouse.get_pos()
             ok = True
             
-            if(x > 200 and x < 600 and y > 150 and y < 550):
+            if(x > 300 and x < 700 and y > 150 and y < 550):
                 i = 0
                 for z in currentRect.formationx:
-                    ix = (x + currentRect.formationx[i] * blockSize) // sizebtwn - 4
+                    ix = (x + currentRect.formationx[i] * blockSize) // sizebtwn - 6
                     iy = (y + currentRect.formationy[i] * blockSize) // sizebtwn - 3
                 
                     if(ix > 7 or iy > 7 or ix < 0 or iy < 0):
@@ -138,7 +160,7 @@ while run:
                         ix = (x + currentRect.formationx[i] * blockSize) // sizebtwn 
                         iy = (y + currentRect.formationy[i] * blockSize) // sizebtwn
                         block = pygame.Rect((ix*sizebtwn, iy*sizebtwn, 50, 50))
-                        map[iy-3][ix-4] = 1
+                        map[iy-3][ix-6] = 1
                         for j in range(8):
                             fullColumn = True
                             for k in range(8):
@@ -165,7 +187,7 @@ while run:
                                     emptyMap = False
                         if (emptyMap and score > 300): score += 300
                         i = i + 1
-                    currentRect = blocks[rand.randint(0, 9)]
+                    currentRect = blocks[rand.randint(0, len(blocks)-1)]
             
             
         if event.type == pygame.QUIT:
@@ -174,4 +196,7 @@ while run:
     pygame.display.update()
             
 pygame.quit()
+
+f = open("highscore.txt", "w")
+f.write(str(hs))
 
